c204901c5bdc4d78439df4b5a0d96f5ef2e7f7c8
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-05-22 10:55:27 +0200

    Bugfix

--- backend/info.php
+++ backend/info.php
@@ -14,13 +14,5 @@ if ($variable_value !== false) {
     echo "$variable_name is not set.";
 }
 
-echo "TEST";
-echo Config::DB_HOST();
 echo Config::DB_NAME();
-echo Config::DB_PASSWORD();
-echo Config::DB_PORT();
-echo Config::DB_USER();
-echo Config::JWT_SECRET();
 
-echo Config::get_env("JWT_SECRET", "testiranje");
-echo getenv("DB_NAME");
--- backend/rest/config.php
+++ backend/rest/config.php
@@ -14,7 +14,19 @@ class Config
 
     public static function DB_NAME()
     {
-        return getenv("DB_NAME") ?? "ibu_startup";
+        $variable_name = 'DB_NAME'; // Change this to the variable you want to check
+
+        // Retrieve the environment variable
+        $variable_value = getenv($variable_name);
+
+        // Check if the environment variable is set
+        if ($variable_value !== false) {
+            echo "$variable_name: $variable_value";
+        } else {
+            echo "$variable_name is not set.";
+        }
+
+        return $variable_value;
     }
 
     public static function DB_PORT()
