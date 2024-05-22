20bd36a106cb77a7529f74c27d8926c8bc9b124b
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-05-22 10:21:17 +0200

    Bugfix

--- backend/info.php
+++ backend/info.php
@@ -1,3 +1,11 @@
 <?
 
 phpinfo();
+
+echo "TEST";
+echo Config::DB_HOST();
+echo Config::DB_NAME();
+echo Config::DB_PASSWORD();
+echo Config::DB_PORT();
+echo Config::DB_USER();
+echo Config::JWT_SECRET();
